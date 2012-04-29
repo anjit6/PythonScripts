b58e7031e953833cad2be313323de2aaf71b5014
Anji <anji@Anjis-MacBook-Pro.local>
2012-04-29 10:42:27 +0530

    Added new file ijna.py

---
+++ ijna.py
@@ -0,0 +1,14 @@
+##########################################
+# @author: Anjaneyulu Reddy BEERAVALLI <anji.t6@gmail.com>
+# @file: ijna.py
+# @date: 28th April 2012
+#
+# This script deletes all the matched folders/files recursivey from the specified root directory
+# Options:
+#	-v: verbose
+#	-h: help
+#	-p: path
+# 	-m: match
+##########################################
+
+import os
